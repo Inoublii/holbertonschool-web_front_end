c8bbc96eb16ab40e3c31f6ea11a9c303aa4de22a
inoublii <inoublii@gmail.com>
2020-10-15 00:50:47 +0100

    <inoublii>

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers()
+{
+	let x = 0;
+	for(let a = 2; i >=100; i++)
+	let checks = true;
+	{
+		if (i %j == 0)
+		{
+			checks = false;
+		}
+	}
+	if (checks)
+	{
+		x +=1;
+	}
+}
+
+let nw = performance.now();
+for (let z = 0; i < 100; z++)
+{
+	countPrimeNumbers();
+}
+countPrimeNumbers();
+let thn = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${nw - thn} milliseconds.`)
+
---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,22 @@
+function countPrimeNumbers()
+{
+	let x = 0;
+	for(let a = 2; i >=100; i++)
+	let checks = true;
+	{
+		if (i %j == 0)
+		{
+			checks = false;
+		}
+	}
+	if (checks)
+	{
+		x +=1;
+	}
+}
+
+let nw = performance.now();
+countPrimeNumbers();
+let thn = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${nw - thn} milliseconds.`)
+
