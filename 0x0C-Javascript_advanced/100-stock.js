ecbdc2f5a47aa08623d3b9fbb7f5b803a6441e4e
inoublii <inoublii@gmail.com>
2020-10-15 01:08:04 +0100

    <inoublii>

---
+++ 0x0C-Javascript_advanced/100-stock.js
@@ -0,0 +1,31 @@
+let stock =
+{
+    macbook: 0,
+    iphone: 4
+}
+function processPayment(itemName)
+{
+    stock[itemName] -= 1;
+    console.log(`Payment is being processed for item ${itemName}`);
+}
+function processError(itemName)
+{
+    console.log(`No more ${itemName} in stock`);
+    console.log("Payment is not being processed");
+}
+function processOrder(itemName, callbackPayment, callbackError)
+{
+    console.log(`Verifying the stock of ${itemName}`);
+	if (stock[itemName] !== undefined && stock[itemName] > 0)
+	{
+        callbackPayment(itemName);
+    }
+	else
+	 {
+        callbackError(itemName);
+    }
+}
+console.log("Please enter the item you would like to purchase (Macbook, iPhone)");
+let name = window.prompt();
+name = name.toUpperCase();
+processOrder(name, processPayment, processError);
---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,27 @@
+function countPrimeNumbers()
+{
+	let x = 0;
+	for(let a = 2; i >=100; i++)
+	let checks = true;
+	{
+		if (i %j == 0)
+		{
+			checks = false;
+		}
+	}
+	if (checks)
+	{
+		x +=1;
+	}
+}
+duration = 0
+setTimeout(() => console.log(`Execution time of calculating prime numbers 100 times was ${duration} milliseconds.`), duration);
+
+let nw = performance.now();
+for (let z = 0; i < 100; z++)
+{
+	countPrimeNumbers();
+}
+countPrimeNumbers();
+let thn = performance.now();
+duration = nw - thn;
